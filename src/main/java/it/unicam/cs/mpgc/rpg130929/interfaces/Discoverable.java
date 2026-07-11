7d9022754a4b59a32116f0a120c537da78ce01ee
Silvio Angelino <silvio.angelino@studenti.unicam.it>
2026-07-11 18:19:50 +0200

    feat: aggiunge metodo default discoveryStatus a Discoverable

--- src/main/java/it/unicam/cs/mpgc/rpg130929/interfaces/Discoverable.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130929/interfaces/Discoverable.java
@@ -4,4 +4,11 @@ package it.unicam.cs.mpgc.rpg130929.interfaces;
 public interface Discoverable {
     boolean isDiscovered();
     void discover();
+
+    // metodo default: stato leggibile per log o debug,
+    // riusa i due metodi astratti sopra senza che ogni
+    // classe debba reimplementarlo
+    default String discoveryStatus() {
+        return isDiscovered() ? "scoperto" : "non scoperto";
+    }
 }
